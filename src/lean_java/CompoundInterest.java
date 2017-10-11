8dfe7f249ef2da63f83eed7501be425af12faac1
jwh5566 <jwh5566@foxmail.com>
2017-10-11 17:23:14 +0800

    add java

---
+++ src/lean_java/CompoundInterest.java
@@ -0,0 +1,51 @@
+package lean_java;
+/** 
+ * @author 作者 : jwh5566
+ * @version 创建时间：2017年10月11日 下午4:21:22 
+ * 类说明 
+ */
+public class CompoundInterest {
+	public static void main(String[] args) {
+		final double STARTRATE = 10;
+		final int NRATES = 6;
+		final int NYEARS = 10;
+		
+		// set interest rates to 10...15%
+		double[] interestRate = new double[NRATES];
+		for (int i=0; i< interestRate.length; i++)
+			interestRate[i] = (STARTRATE + i)/100.0;
+		
+		double[][] balances = new double[NYEARS][NRATES];
+		
+		// set initial balances to 10000
+		for (int j = 0; j < balances[0].length; j++)
+			balances[0][j] = 10000;
+		
+		// computer interest for future years
+		for (int i = 1; i< balances.length; i++)
+		{
+			for (int j = 0; j < balances[i].length; j++)
+			{
+				// get the last year balance from previous row
+				double oldBalance = balances[i-1][j];
+				// compute interest
+				double interest = oldBalance * interestRate[j];
+				//compute this year's balances
+				balances[i][j] = oldBalance + interest;
+			}
+		}
+		
+		// print one row for the interest rates
+		for (int j=0; j<interestRate.length; j++)
+			System.out.printf("%9.0f%%", 100 * interestRate[j]);
+		System.out.println();
+		
+		//print balance table
+		for (double[] row : balances)
+		{
+			for (double b : row)
+				System.out.printf("%10.2f", b);
+			System.out.println();
+		}
+	}
+}
